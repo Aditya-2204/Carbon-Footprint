47d0e69d5d48848ad414516651182b03b42ece3c
Aditya <83343763+Aditya-2204@users.noreply.github.com>
2023-09-09 14:22:42 -0500

    Add files via upload

---
+++ Carbon Footprint.py
@@ -0,0 +1,8 @@
+'''
+Aditya Chakraborty
+8/22
+Carbon Footprint calculator
+'''
+
+miles = float(input("How many miles do you drive to school? "))
+print(f"Your carbon footprint from driving to and from school is {round(miles*04.11, 2)}kg  per week".format(":.2f"))
